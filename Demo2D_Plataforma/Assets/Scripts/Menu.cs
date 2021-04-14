a03080d86de77110e5735302cba1e9915b43a4eb
A01750170 <A01750170@itesm.mx>
2021-04-14 12:21:51 -0500

    Revert "l"
    This reverts commit db827a7a47a4415e9f99e9cee3b4f0fd9a46fa15.

--- Demo2D_Plataforma/Assets/Scripts/Menu.cs
+++ Demo2D_Plataforma/Assets/Scripts/Menu.cs
@@ -1,27 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 /*
 *Controla el menu
 */
 public class Menu : MonoBehaviour
-
 {
-    public Image imagenFondo;
     //cambiar escena
-    private IEnumerator CambiarEscena(){
-        yield return new WaitForSeconds(0.7f);
-        SceneManager.LoadScene("EscenaMapa");
-    }
     public void IniciarJuego(){
-        //FadeOut
-        imagenFondo.canvasRenderer.SetAlpha(0);
-        imagenFondo.gameObject.SetActive(true);
-        imagenFondo.CrossFadeAlpha(1,0.7f,true);
-        StartCoroutine(CambiarEscena());
-        //SceneManager.LoadScene("EscenaMapa");
+        SceneManager.LoadScene("EscenaMapa");
     }
 
     public void SalirJuego(){
