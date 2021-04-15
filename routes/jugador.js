779dcaddbd59e831e55b4c5ad833591cd6bb82b3
Jacqueline Zavala <a01750480@itesm.mx>
2021-04-14 21:10:46 -0500

    Eliminación de archivo sequelize.js
    Se borró el archivo "sequelize.js" dado que estaba en blanco.

--- routes/jugador.js
+++ routes/jugador.js
@@ -7,7 +7,7 @@ const JugadorController = require('../controllers/jugador')
 //Registra un nuevo usuario en la base de datos desde el juego
 router.post('/registro',JugadorController.postRegistroJugador);
 
-//Obtiene los datos del usuario cuando inicia sesion
+//Obtiene los datos del usuario cuando inicia sesión
 router.post('/iniciarSesion',JugadorController.postIniciarSesion);
 
 //Registra la fecha y hora de cuando termina el juegue
